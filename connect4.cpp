f8d187cd2b9872cc9cfd8bf0a4c0a536b2df674f
momox369 <salamehmaurice13@gmail.com>
2023-01-06 20:36:12 +0200

    Initialize a 6 x 7 board

--- connect4.cpp
+++ connect4.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+#include <windows.h>
+#include <stdlib.h>
+using namespace std;
+#define ROWS 6
+#define COLS 7
+#define red "\u001b[31m"
+#define reset "\u001b[0m"
+#define yellow "\u001b[33m"
+
+char** initializeBoard();
+
+
+
+int main() {
+    //creating the board of the game
+    char** board = initializeBoard();
+    cout << "Board initialized!";
+    
+    return 0;
+}
+
+char** initializeBoard(){
+    char** board = new char*[ROWS];
+    for (int i = 0; i < ROWS; i++){
+        board[i] = new char[COLS];
+    }
+    for (int i = 0; i < ROWS; i++){
+        for (int j = 0; j < COLS; j++){
+            board[i][j] = 'O';
+        }
+    }
+    return board;
+}
+
+
 
